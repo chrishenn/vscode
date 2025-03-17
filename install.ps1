27de7942a4df00534a48313670757b6c4d494853
Chris Henn <chenn@alum.mit.edu>
2025-03-16 21:31:45 -0500

    add git history; fix scoop installer

--- install.ps1
+++ install.ps1
@@ -1,4 +1,8 @@
-iex "& {$(irm get.scoop.sh)} -RunAsAdmin | out-null"
+pushd /
+
+irm get.scoop.sh -outfile 'scoop.ps1'
+.\scoop.ps1 -RunAsAdmin | out-null
+rm scoop.ps1
 
 scoop install vscode
 code --install-extension yathink3.carbon-react-color-theme --force
@@ -6,11 +10,11 @@ code --install-extension esbenp.prettier-vscode --force
 code --install-extension be5invis.vscode-custom-css --force
 code --install-extension apility.beautify-blade --force
 code --install-extension isudox.vscode-jetbrains-keybindings --force
+code --install-extension donjayamanne.githistory --force
 
 scoop bucket add nerd-fonts
 scoop install firacode
 
-pushd /
 git clone git@github.com:chrishenn/vscode.git
 cd vscode
 cp /vscode/settings.json "$HOME\scoop\apps\vscode\current\data\user-data\User\settings.json" -force
--- uninstall.ps1
+++ uninstall.ps1
@@ -3,6 +3,7 @@ code --uninstall-extension esbenp.prettier-vscode --force
 code --uninstall-extension be5invis.vscode-custom-css --force
 code --uninstall-extension apility.beautify-blade --force
 code --uninstall-extension isudox.vscode-jetbrains-keybindings --force
+code --uninstall-extension donjayamanne.githistory --force
 scoop uninstall vscode -p
 
 scoop uninstall firacode -p
